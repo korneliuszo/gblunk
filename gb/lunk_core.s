81db71b68bc82ffca1caa028a4ed42a159dcad4a
Korneliusz Osmenda <korneliuszo@gmail.com>
2023-03-22 16:39:42 +0100

    Optimize code from insights from internet discussion

--- gb/lunk_core.s
+++ gb/lunk_core.s
@@ -5,68 +5,57 @@ _SC_REG      = 0xFF02    ; Serial IO control register
 
 	.area _RAMCODE
 
-.macro READ_SIO ?notyet
+; HL must contain _SC_REG address
+.macro WAIT_SIO ?notyet
 notyet:
-	ldh	a, (_SC_REG + 0)
-	rlca
-	jr	C, notyet
-	ldh	a, (_SB_REG + 0)
-	ld c, a
-	ld	a, #0x80
-	ldh	(_SC_REG + 0), a
+        bit     7, (hl)
+        jr      nz, notyet
 .endm
-
-.macro WRITE_SIO ?notyet
-notyet:
-	ldh	a, (_SC_REG + 0)
-	rlca
-	jr	C, notyet
-	ld a, c
-	ldh	(_SB_REG + 0), a
-	ld	a, #0x80
-	ldh	(_SC_REG + 0), a
+.macro READ_SIO
+        WAIT_SIO
+        ldh     a, (_SB_REG)
+        ld      (hl), #0x80
 .endm
-
-read:
-	cp #0x52 ; 'R'
-	jr	NZ, _lunk_core
-	READ_SIO
-	ld b, c
-	READ_SIO
-	ld h, c
-	READ_SIO
-	ld l, c
-read_loop:
-	ld a,(HLI)
-	ld c, a
-	WRITE_SIO
-	dec b
-	jr NZ, read_loop
-	notyet:
-	ldh	a, (_SC_REG + 0)
-	rlca
-	jr	C, notyet
-	jr loop
-
-_lunk_core:
-	ld	a, #0x80
-	ldh	(_SC_REG + 0), a
+.macro WRITE_SIO
+        WAIT_SIO
+        ldh     (_SB_REG), a
+        ld      (hl), #0x80
+.endm
+_lunk_core::
+        ld      hl, #_SC_REG
+        ld      (hl), #0x80
 loop:
-	READ_SIO
-	ld a, c
-	cp #0x57 ; 'W'
-	jr	NZ, read
-	READ_SIO
-	ld b, c
-	READ_SIO
-	ld h, c
-	READ_SIO
-	ld l, c
+        READ_SIO
+        cp      #'W'
+        jr      NZ, read
+        READ_SIO
+        ld      c, a  ; c == count
+        READ_SIO
+        ld      d, a
+        READ_SIO
+        ld      e, a  ; de == address
 write_loop:
-	READ_SIO
-	ld a, c
-	ld (HLI), a
-	dec b
-	jr NZ, write_loop
-	jr loop
-
+        READ_SIO
+        ld      (de), a
+        inc     de
+        dec     c
+        jr      nz, write_loop
+        jr      loop
+read:
+        cp      #'R'
+        jr      nz, loop
+        READ_SIO
+        ld      c, a
+        READ_SIO
+        ld      d, a
+        READ_SIO
+        ld      e, a
+read_loop:
+        ld      a, (de)
+        WRITE_SIO
+        inc     de
+        dec     c
+        jr      nz, read_loop
+        WAIT_SIO
+        ld      (hl), #0x80
+        jr      loop
